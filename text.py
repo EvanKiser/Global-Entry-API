db59ddd8ac96ab9ed4443600aee5ca3ef0bd1e1f
Evan Kiser <kiser.evan1@gmail.com>
2022-10-27 20:25:42 -0700

    make plan for texting service

---
+++ text.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    # get users from db
+    # create dict where key is location_id and value is list of user phone numbers
+    # for each location_id in the dict
+    #   # check for new appts
+    #   # send text about new appt if new appt found
