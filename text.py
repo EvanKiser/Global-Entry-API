96e072865496f21041b5accc6c092dff6d0c433f
Evan Kiser <kiser.evan1@gmail.com>
2022-10-27 22:30:35 -0700

    update users to locations dict

--- text.py
+++ text.py
@@ -1,5 +1,6 @@
 import json
 from urllib import request
+from appointments import check_for_appointments
 from dotenv import load_dotenv
 import os
 from twilio.rest import Client
@@ -14,14 +15,13 @@ client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 def users_dict_to_locations_dict(users_dict):
     locations_dict = {}
-    for item in users_dict:
-        for _, user in item.items():
-            user_phone_number = user["phone"]
-            for location in user["locations"]:
-                if location in locations_dict:
-                    locations_dict.append(user_phone_number)
-                else:
-                    locations_dict[location] = [user_phone_number]
+    for user in users_dict:
+        user_phone_number = user["phone"]
+        for location in user["locations"]:
+            if location in locations_dict:
+                locations_dict[location].append(user_phone_number)
+            else:
+                locations_dict[location] = [user_phone_number]
     return locations_dict
 
 if __name__ == '__main__':
@@ -29,8 +29,12 @@ if __name__ == '__main__':
     data = response.read()
     users_dict = json.loads(data)
     locations_dict = users_dict_to_locations_dict(users_dict)
-    for location_code, location_phone_numbers in locations_dict.items():
-        pass
+    for location_id, phone_numbers in locations_dict.items():
+        appointments = check_for_appointments(location_id)
+        print(appointments)
+        for appt in appointments:
+            pass
+            #Send text about appointments
         # CAN"T TEXT PPL THE SAME THING
         #   check for new appts
         #   send text about new appt if new appt found
