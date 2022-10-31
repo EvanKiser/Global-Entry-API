76cde9d0da703d0d2006ccee0527a1c93d7b2aa9
Evan Kiser <kiser.evan1@gmail.com>
2022-10-31 10:28:22 -0700

    app run

--- app.py
+++ app.py
@@ -199,6 +199,6 @@ def delete_user(id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
-        db.session.commit()
-        # app.run()
+        # db.create_all()
+        # db.session.commit()
+        app.run()
