b270e75fe802571dc25296fd5566d07fa5bf9ef3
Evan Kiser <kiser.evan1@gmail.com>
2022-11-08 14:59:53 -0800

    print request type

--- app.py
+++ app.py
@@ -162,6 +162,7 @@ class User(db.Model):
 
 @app.route('/user', methods = ['POST'])
 def add_user():
+    print(request.method)
     print(request.json)
     data = request.json['data']
     if request.method == 'POST':
