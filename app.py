0d781d1bcc97405d168bf6688efd5523ab131d8c
Evan Kiser <kiser.evan1@gmail.com>
2022-11-08 14:43:52 -0800

    load env vars

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import os
 from mutable import MutableList
 from twilio.rest import Client
 
+load_dotenv()
+
 ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
