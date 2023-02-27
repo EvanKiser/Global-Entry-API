42da6ddc4145d8cccbe74ecb4d0f2fe329212565
Evan Kiser <kiser.evan1@gmail.com>
2023-02-26 20:55:57 -0800

    itll work now

--- app.py
+++ app.py
@@ -507,7 +507,9 @@ def paid():
         session = event['data']['object']
         user_id = session['client_reference_id'][1:-1]
         user = User.query.get(user_id)
-        send_reminder_to_user(user.id, user.phone, user.city, user.state)
+        location_id = user.locations[0]
+        city, state = map_id_to_location(location_id)
+        send_reminder_to_user(user.id, user.phone, city, state)
         send_reminder_to_me(user.id, city, state)
         return resp
 
