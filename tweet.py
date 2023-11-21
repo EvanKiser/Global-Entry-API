3ebfd176dddcd812d278167b2a2968c8a73fba1b
Evan Kiser <evan@kiser.io>
2023-11-21 13:13:50 -0500

    Consolidate tweets into one cron

--- tweet.py
+++ tweet.py
@@ -15,10 +15,10 @@ import tweepy
 
 API_URL = os.getenv("API_URL") if os.getenv("ENV") != 'dev' else 'http://127.0.0.1:5000'
 
-API_KEY = os.getenv("TWITTER_API_KEY")
-API_KEY_SECRET = os.getenv("TWITTER_API_SECRET")
-ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+API_KEY_1 = os.getenv("TWITTER_API_KEY")
+API_KEY_SECRET_1 = os.getenv("TWITTER_API_SECRET")
+ACCESS_TOKEN_1 = os.getenv("TWITTER_ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET_1 = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 NOTIF_MESSAGE = 'New Global Entry interview appointment open in {display_name}: {timestamp}\n\nReceive text message alerts for interviews near you for FREE. Link in bio. GlobalEntryScan.com'
 
@@ -33,12 +33,12 @@ def get_location_display_name(location_id):
         if location["id"] == location_id:
             return location["display_name"]
 
-def send_tweet(location_id, timestamp):
+def send_tweet(location_id, timestamp, api_key, api_key_secret, access_token, access_token_secret):
     client = tweepy.Client(
-        consumer_key=API_KEY,
-        consumer_secret=API_KEY_SECRET,
-        access_token=ACCESS_TOKEN,
-        access_token_secret=ACCESS_TOKEN_SECRET,
+        consumer_key=api_key,
+        consumer_secret=api_key_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
     )
 
     tweet_msg = NOTIF_MESSAGE.format(
@@ -59,30 +59,52 @@ def send_tweet(location_id, timestamp):
     return True
 
 if __name__ == '__main__':
-    keep_running = True
-    while keep_running:
-        with open('locations.json') as locations_path:
-            locations = json.load(locations_path)
-        random_location = locations[random.randint(0, len(locations)-1)]
-        random_location_id = random_location["id"]
-        appointments = get_appointments(random_location_id, 8)
-        if appointments == []:
-            continue 
-        location = get_location_data(random_location_id)
+    for i in range(4):
+        if i == 0:
+            API_KEY = os.getenv("TWITTER_API_KEY_1")
+            API_KEY_SECRET = os.getenv("TWITTER_API_SECRET_1")
+            ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN_1")
+            ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET_1")
+        if i == 1:
+            API_KEY = os.getenv("TWITTER_API_KEY_2")
+            API_KEY_SECRET = os.getenv("TWITTER_API_SECRET_2")
+            ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN_2")
+            ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET_2")
+        if i == 2:
+            API_KEY = os.getenv("TWITTER_API_KEY_3")
+            API_KEY_SECRET = os.getenv("TWITTER_API_SECRET_3")
+            ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN_3")
+            ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET_3")
+        if i == 3:
+            API_KEY = os.getenv("TWITTER_API_KEY_4")
+            API_KEY_SECRET = os.getenv("TWITTER_API_SECRET_4")
+            ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN_4")
+            ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET_4")
+            
+        keep_running = True
+        while keep_running:
+            with open('locations.json') as locations_path:
+                locations = json.load(locations_path)
+            random_location = locations[random.randint(0, len(locations)-1)]
+            random_location_id = random_location["id"]
+            appointments = get_appointments(random_location_id, 8)
+            if appointments == []:
+                continue 
+            location = get_location_data(random_location_id)
 
-        # cast past appointments into datetime format
-        past_appointments = format_past_appointments(location.past_appointments)
+            # cast past appointments into datetime format
+            past_appointments = format_past_appointments(location.past_appointments)
 
-        for appointment in appointments:
-            if appointment['active'] <= 0:
-                continue
-            date = format_ttp_date(appointment['timestamp'])
-            # Check if we have tweeted this appointment before
-            if date in past_appointments:
-                continue
-            timestamp = date.strftime(MESSAGE_TIME_FORMAT)
-            if send_tweet(location.id, timestamp):
-                add_tweeted_appointment_to_db(location.id, timestamp)
-                keep_running = False
-                break
+            for appointment in appointments:
+                if appointment['active'] <= 0:
+                    continue
+                date = format_ttp_date(appointment['timestamp'])
+                # Check if we have tweeted this appointment before
+                if date in past_appointments:
+                    continue
+                timestamp = date.strftime(MESSAGE_TIME_FORMAT)
+                if send_tweet(location.id, timestamp, API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
+                    add_tweeted_appointment_to_db(location.id, timestamp)
+                    keep_running = False
+                    break
         
